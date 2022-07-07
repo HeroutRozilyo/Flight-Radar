484666e55d05da073b7d9e02098bf6633df8e661
HeroutRozilyo <heroot12@gmail.com>
2022-07-07 11:30:14 +0300

    Flight model

---
+++ FlightModel/HelperClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightModel
+{
+    public class HelperClass
+    {
+        //convert from unix time to humane datetime
+        public DateTime GetDateTimeFromEpohc(double Epoch)
+        {
+            DateTime start = new DateTime(1970, 1, 1, 0, 0, 0, 0);// start epoch time
+            start = start.AddSeconds(Epoch);//add the second to the start time
+            return start;
+        }
+
+    }
+}
