c493f2518160840b86a5e095bacf59133fdba6ce
da077 <da077@DESKTOP-HV19KNK>
2022-08-02 11:53:22 +0300

    hhh

---
+++ HebDates1/HebAdapter.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HebDates
+{
+    public class HebAdapter
+    {
+        //public bool IfHoliday(DateTime date)
+        //{
+        //    ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+        //    ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+
+        //    System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+
+        //    using (var webClient = new System.Net.WebClient())
+        //    {
+        //        var yyyy = date.ToString("yyyy");
+        //        var mm = date.ToString("MM");
+        //        var dd = date.ToString("dd");
+        //        string url = $"https://www.hebcal.com./converter?cfg=json&date={yyyy}-{mm}-{dd}&g2h=1&strict=1";
+
+
+        //        var json =  webClient.DownloadString(url);
+        //        HebModel.Root root = JsonConvert.DeserializeObject<HebModel.Root>(json);
+
+        //        if (root.events[0].Contains("Erev"))
+        //            return true;
+        //        return false;
+
+        //    }
+
+        //}
+
+
+
+        public async Task<bool> IfHoliday(DateTime date)
+        {
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+            using (var webClient = new System.Net.WebClient())
+            {
+                var yyyy = date.ToString("yyyy");
+                var mm = date.ToString("MM");
+                var dd = date.ToString("dd");
+                string url = $"https://www.hebcal.com./converter?cfg=json&date={yyyy}-{mm}-{dd}&g2h=1&strict=1";
+
+                var json = await webClient.DownloadStringTaskAsync(url);
+                HebModel.Root root = JsonConvert.DeserializeObject<HebModel.Root>(json);
+
+                if (root.events[0].Contains("Erev"))///
+                    return true;////
+                return false;
+
+            }
+            /////////////////
+            ///
+
+        }
+
+
+    }
+}
---
+++ Weather1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Weather1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Weather1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e2b8865a-778e-43ec-aee2-2526fd3bf7a8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
